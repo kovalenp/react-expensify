149f9d864eb4004f51a61d6c735674ef31b58469
Pavel Kovalenko <pavel@kovalenko.cz>
2018-08-14 15:05:28 +0200

    Add travis CI

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 const path = require('path');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env) => {
   const devMode = env !== 'production';
-  
+
   return {
     mode: 'development',
     entry: './src/app.js',
     output: {
       path: path.join(__dirname, 'public'),
       filename: 'bundle.js',
-      publicPath: '/'
+      publicPath: '/',
     },
     module: {
       rules: [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          use: ['babel-loader']
+          use: ['babel-loader'],
         },
         {
           test: /\.js$/,
           exclude: /node_modules/,
-          use: ['babel-loader', 'eslint-loader']
+          use: ['babel-loader', 'eslint-loader'],
         },
         {
           use: [
@@ -40,12 +40,12 @@ module.exports = (env) => {
       new HtmlWebpackPlugin(({
         title: 'Expensify App',
         template: './src/index.html',
-      }))
+      })),
     ],
     devtool: devMode ? 'inline-source-map' : 'source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
     },
-}
+  };
 };
